63ae7eaf2fdea4ed8a84f0527ac670a19d17def6
Sotiris Ioannou <sotiris@jsmate.net>
2020-04-16 00:31:46 +0200

    Display a random card

--- src/my_project/pages/about.cljs
+++ src/my_project/pages/about.cljs
@@ -1,7 +1,12 @@
-(ns my-project.pages.about)
+(ns my-project.pages.about
+  (:require [goog.string :as gstring]))
+
+(defn get-random-card 
+  "Returns a random card from the collection passed to this function"
+  [col] (rand-nth col))
 
 (defn about-page []
       [:div
-        [:h1 "This is the about page"]
-       ]
-      )
+       [:h1 "This is the about page"]
+       (let [playing-cards ["&#9824" "&#9827" "&#9829" "&#9830"]]
+         [:p (gstring/unescapeEntities (get-random-card playing-cards))])])
