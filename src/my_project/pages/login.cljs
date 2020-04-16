c73ac3f66f78ab3f8524e0b3ca46e3d1191c5cd5
Sotiris Ioannou <sotiris@jsmate.net>
2020-04-17 01:30:10 +0200

    Login form

--- src/my_project/pages/login.cljs
+++ src/my_project/pages/login.cljs
@@ -1,15 +1,31 @@
 (ns my-project.pages.login
   (:require
-   [my-project.state.state :as state :refer [app-state]]))
+    [my-project.state.state :as state :refer [app-state]]
+    [reagent-forms.core :refer [bind-fields]]
+    [reagent.core :as r]))
 
 (defn on-button-click []
-  "On login button click fake login by adding a user so we can login in"
-  (swap! app-state assoc :user "Sotiris")
-  (println @app-state))
+      "On login button click fake login by adding a user so we can login in"
+      (swap! app-state assoc :user "Sotiris")
+      (println @app-state))
+
+(def form-template
+  [:div
+   [:div[:label "username: "
+        [:input.form-control {:field :text :id :username}]]]
+   [:div[:label "password: "
+        [:input.form-control {:field :password :id :pass}]]]])
+
+(defn login-form []
+      (let [doc (r/atom {:username "Sotiris" :pass "1234"})]
+           [bind-fields form-template doc
+            (fn [id path value]
+                (println @doc))]))
+
 
 (defn login-page []
-  (if (not (= js/window.location.pathname "/login"))
-    (set! js/window.location.href "/login"))
-  [:div
-   [:h1 "This is the login page"]
-   [:input {:type "button" :value "Fake login" :on-click on-button-click}]])
+      (if (not (= js/window.location.pathname "/login"))
+        (set! js/window.location.href "/login"))
+      [:div
+       [:h1 "This is the login page"]
+       [login-form]])
